b912cb01c1acd2ba16c14239bb2ffc5211ecddc5
Andrew Brookins <andrew.brookins@redislabs.com>
2020-12-08 23:15:58 -0800

    Fix thruster tests

--- tests/test_spaceship.py
+++ tests/test_spaceship.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
+from dataclasses import dataclass
 from spaceship import keys
 from pytest import fixture
 from redis import Redis
 
 from spaceship.models import Direction, Velocity
-from spaceship.spaceship import DictShip, RedisShip, DictDeck, DictThruster, ListEventLog, RedisThruster
+from spaceship.spaceship import DictShip, RedisShip, DictDeck, DictThruster, ListEventLog, RedisThruster, NoCapacityError
 
 EARTH_GRAVITY = 9.8
 TWO_MILLION_KG = 2e6
@@ -26,13 +27,13 @@ def dict_ship():
         'base_mass_kg': TWO_MILLION_KG,
         'current_mass_kg': TWO_MILLION_KG,
         'current_gravity': EARTH_GRAVITY,
+        'decks': {}
     }
     return DictShip(data,
                     base_mass_kg=TWO_MILLION_KG,
                     event_log=ListEventLog(),
-                    decks=[DictDeck('main', 1000)],
-                    aft_thruster=DictThruster('aft', data),
-                    forward_thruster=DictThruster('forward', data))
+                    decks=[DictDeck('main', data, 1000)],
+                    thruster=DictThruster(data))
 
 
 @fixture
@@ -42,19 +43,18 @@ def redis_ship(redis):
     return RedisShip(redis,
                      base_mass_kg=TWO_MILLION_KG,
                      event_log=ListEventLog(),
-                     decks=[DictDeck('main', 1000)],
-                     aft_thruster=RedisThruster('aft', redis),
-                     forward_thruster=RedisThruster('forward', redis))
+                     decks=[DictDeck('main', {}, 1000)],
+                     thruster=RedisThruster(redis))
 
 
 def test_accelerate_dict_ship(dict_ship: DictShip):
     dict_ship.accelerate(Velocity(500, Direction.N))
 
-    assert len(dict_ship.event_log) == 6
-    for event in dict_ship.event_log.events[0:4]:
+    assert len(dict_ship.event_log) == 10
+    for event in dict_ship.event_log.events[0:8]:
         assert event.data == {'fuel_burned': 2}
-    assert dict_ship.event_log.events[5].data == {
-        'fuel_burned': 1.5740740740740726
+    assert dict_ship.event_log.events[9].data == {
+        'fuel_burned': 0.27485380116959135
     }
 
     assert round(dict_ship.data['speed_kmh'][Direction.N.value]) == 500.0
@@ -63,12 +63,27 @@ def test_accelerate_dict_ship(dict_ship: DictShip):
 def test_accelerate_redis_ship(redis_ship: RedisShip):
     redis_ship.accelerate(Velocity(500, Direction.N))
 
-    assert len(redis_ship.event_log) == 6
-    for event in redis_ship.event_log.events[0:4]:
+    assert len(redis_ship.event_log) == 10
+    for event in redis_ship.event_log.events[0:8]:
         assert event.data == {'fuel_burned': 2}
-    assert redis_ship.event_log.events[5].data == {
-        'fuel_burned': 1.5740740740740726
+    assert redis_ship.event_log.events[9].data == {
+        'fuel_burned': 0.27485380116959135
     }
 
-    current_speed = float(redis_ship.data[keys.ship_current_speed(Direction.N)])
+    current_speed = float(redis_ship.data[keys.ship_current_speed(Direction.N.value)])
     assert round(current_speed) == 500.0
+
+
+def test_load_dict_deck(dict_ship: DictShip):
+    @dataclass
+    class Animal:
+        name: str
+        weight_kg:float
+
+    mouse = Animal(name="mouse", weight_kg=0.25)
+
+    assert dict_ship.weight_kg == TWO_MILLION_KG * EARTH_GRAVITY
+
+    dict_ship.store('main', mouse)
+
+    assert dict_ship.weight_kg == (TWO_MILLION_KG + 0.25) * EARTH_GRAVITY
